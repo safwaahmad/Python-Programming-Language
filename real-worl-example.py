b22845b2fea9d53232910c29cbfd8d8bbfabbf5f
safwaahmad <sahmedsaffi@gmail.com>
2024-07-21 13:47:45 +0500

    add

---
+++ real-worl-example.py
@@ -0,0 +1,5 @@
+#You working as softwarae developer. One of your customer is planning to make  50% discount on the products that he is selling so he asked you to design a program that gives the price of the any product after 50% discount
+discount = 50/100
+orignal= int(input("Enter the value :"))
+sales=orignal*discount
+print("The discounted price is:",int(sales),sep='$')
