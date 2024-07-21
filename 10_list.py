e14f5d3c36797dde0825466b4cab808b815819e1
safwaahmad <sahmedsaffi@gmail.com>
2024-07-21 21:51:52 +0500

    add

---
+++ 10_list.py
@@ -0,0 +1,11 @@
+#list
+#list are used to store multiple iteems or attribute in single variable or entitiy
+#list it can be change
+#store multiple datatype
+
+example = ["sunady","monday","tuesday"]
+print(example)
+
+dif_data_type =[1,2.9,"safwan",True]
+print(dif_data_type)
+
