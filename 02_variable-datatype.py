f05369d36cec7a07258d5a6e3c3cd30538ed8f96
safwaahmad <sahmedsaffi@gmail.com>
2024-07-21 13:58:07 +0500

    add

--- 02_variable-datatype.py
+++ 02_variable-datatype.py
@@ -1,4 +1,11 @@
 #Variables
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+o = str(3)    # x will be '3'
+p = int(3)    # y will be 3
+s = float(3)  # z will be 3.0
+#------------------------------
 a= 10
 b= "safwan"
 print(a)  #seprate show in line
