c334ca0f0a2cf126bedd060445e9c855b4bcff40
safwaahmad <sahmedsaffi@gmail.com>
2024-07-21 22:47:49 +0500

    add

--- 10_list.py
+++ 10_list.py
@@ -1,19 +1,25 @@
 #list
 #list are used to store multiple iteems or attribute in single variable or entitiy
-#list it can be change
+#list it can be changeable (MUTABLE)
 #store multiple datatype
 
 example = ["sunady","monday","tuesday"]
 print(example)
-
+print("-------------------------------")
 dif_data_type =[1,2.9,"safwan",True]
 print(dif_data_type)
 print(type(dif_data_type))
 print(len(dif_data_type))
-
-
+print("-------------------------------")
 nested_list= ["python", [1,2,3], "safwan"]
 print(nested_list)
 print(type(nested_list))
 print(len(nested_list))
-print(nested_list[0])
+print(nested_list[0]) #by indexing
+print(nested_list[:3])#by slicing
+print(nested_list[0]=="python") 
+nested_list[0]="java"
+print(nested_list)
+print(nested_list[0]=="python") 
+print("-------------------------------")
+
