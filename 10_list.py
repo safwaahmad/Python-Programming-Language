6dcd9189a8354ea7ff3d20765b3b3ffa5b5908b9
safwaahmad <sahmedsaffi@gmail.com>
2024-07-21 22:32:52 +0500

    add

--- 10_list.py
+++ 10_list.py
@@ -8,4 +8,12 @@ print(example)
 
 dif_data_type =[1,2.9,"safwan",True]
 print(dif_data_type)
+print(type(dif_data_type))
+print(len(dif_data_type))
 
+
+nested_list= ["python", [1,2,3], "safwan"]
+print(nested_list)
+print(type(nested_list))
+print(len(nested_list))
+print(nested_list[0])
