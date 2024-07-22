f6a5ae308ff63600910fae114b5776628e33127d
safwaahmad <sahmedsaffi@gmail.com>
2024-07-22 09:34:56 +0500

    add

---
+++ 11_tuple.py
@@ -0,0 +1,10 @@
+#tuple is similar to list but it is imutable,can't be change
+tuple =(10,20,30)
+print(tuple)
+tuple1 = (1,'a',2,'b')
+print(tuple1)
+tuple2=([1,2,3],["safwan","ahmad","saffi"])
+print(tuple2)
+print(type(tuple2))
+print(len(tuple2))
+#perfome all the opreation like on list
---
+++ 12_ set.py
@@ -0,0 +1,18 @@
+#set are unorder but can remove and change!
+#random and unique ,not duplicate allowed
+y={}
+print(type(y))
+#first methid ot define a set
+x= {1}
+print(x)
+print(type(x))
+#second methof to define a set
+z = set()
+print(z)
+set1={1,2,3,1,"safwan","saffi",True,False,4,1,False}
+print(set1)
+
+
+
+
+
