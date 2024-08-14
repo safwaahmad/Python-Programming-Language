e925ff32b410ccf4f088c0c6acb58b61663c27eb
safwaahmad <sahmedsaffi@gmail.com>
2024-08-14 11:20:55 +0500

    add

--- 02_variable-datatype.py
+++ 02_variable-datatype.py
@@ -1,27 +1,38 @@
-#Variables
+print("Variables")
 x = 4       # x is of type int
 x = "Sally" # x is now of type str
 print(x)
 o = str(3)    # x will be '3'
 p = int(3)    # y will be 3
 s = float(3)  # z will be 3.0
-#------------------------------
+print("------------------------------")
 a= 10
 b= "safwan"
 print(a)  #seprate show in line
 print(b)  #seprate show in line
 print(a,b)#show same in line
-#-------------------------------
-#Data-Types
+print("------------------------------")
+print("Data-Types")
 a=10
 b=20.9
 c="string"
 d=True
 e=None
-#--------------------------------
-#printing the variable along with data-type
+print("------------------------------")
+print("printing the variable along with data-type")
 print(type(a),a)
 print(type(b),b)
 print(type(c),c)
 print(type(d),e)
 print(type(e),d)
+print("------------------------------")
+print("Print list,tuple,sets and distnory")
+print("Display the list")
+print([1,2,3,4,5])
+print(type([1,2,3,4,5]))
+print("Display the tuple")
+print((1,2,3,4,5))
+print(type((1,2,3,4,5)))
+print("Display the sets")
+print("Display the distnory")
+
