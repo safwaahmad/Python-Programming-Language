68e7fa70369d045c672c6e0d806577e9c53f1cc5
safwaahmad <sahmedsaffi@gmail.com>
2024-08-14 12:17:31 +0500

    add

--- 03_opreators.py
+++ 03_opreators.py
@@ -1,11 +1,11 @@
-#Arithmetic Opreator
+print("-------Arithmetic Opreator-------")
 a=6
 b=9
 print("The  value of 6+9 is",6+9)
 print("The  value of 6-9 is",a-b)
 print("The  value of 6*9 is",a*b)
 print("The  value of 6/9 is",a/b)
-#Assignment  Opreator
+print("-------Assignment Opreator--------")
 A=10
 B=20
 C=30
@@ -15,13 +15,13 @@ B-=2
 C*=2
 D/=2
 print(A,B,C,D)
-#Comparision Opreator
+print("--------Comparision Opreator------")
 print(A<B)
 print(A<C) 
 print(A<D)
 print(A<10)
 print(A!=12)
-#Logical Opreator
+print("-----------Logical Opreator--------")
 print("The vlaues are",A and B)
 bool1=True
 bool2=False
@@ -29,3 +29,18 @@ print("The value of bool1 and bool2 are :", bool1 and bool2)
 print("The value of bool1 or bool2 are : ", bool1  or bool2)
 print("The value of bool1 is:",  bool1 )
 print("The value of  not bool1  :",  (not bool1 ))
+print("-----------Identity Opreator--------")
+var1 = ["apple","banana"]
+var2 = ["apple","banana"]
+var3 = ["apple","banana"]
+var1 = var3
+print(var1 is var2)
+print(var1 is var3)
+print(var1 == var2)
+print("-----------MmeberShip Opreator--------")
+#in/not in
+print(1 in [1,2,3])
+print("a" not in "seffi")
+
+
+
