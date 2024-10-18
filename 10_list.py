e4045198d608c92a09bc876af9761b6a40c3ed68
safwaahmad <sahmedsaffi@gmail.com>
2024-10-18 19:59:02 +0500

    update

--- 10_list.py
+++ 10_list.py
@@ -4,7 +4,7 @@
 #store multiple datatype
 #nested - list  
 #different data-type in same list
-
+##<List is a collection which is ordered and changeable. Allows duplicate members>
 
 example = ["sunady","monday","tuesday"]
 print(example)
@@ -27,6 +27,38 @@ print(nested_list[0]=="python")
 print(dif_data_type[0:3:1]) # 1 is the space b/w 0:3
 dif_data_type.append(5) #in last
 print(dif_data_type.append)
+print("------Python - Change List Items------")
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+thislist.insert(0, "watermelon")
+print(thislist)
+print("------Python - Add List ------")
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange") ##--------------Append Items
+print(thislist)
+#--The insert() method inserts an item at the specified index:
+print("----Insert Items----")
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange") ##-----------Insert Items
+print(thislist)
+#To append elements from another list to the current list, use the extend() method.
+print("-----Extend List-----")
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)  ##-----------Extend list
+print(thislist)
+print("---------REMOVE-------")
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)  ##Remove Specified Index
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]  ##delete the list
+print(thislist)
 print("-------------------------------")
 #add string to list
 name = "safwan"  #string 
--- 11_tuple.py
+++ 11_tuple.py
@@ -1,4 +1,6 @@
 #tuple is similar to list but it is imutable,can't be change
+#Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+
 tuple =(10,20,30)
 print(tuple)
 tuple1 = (1,'a',2,'b')
